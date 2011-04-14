1099dbad666f89e4b8b9b01a161f4e3fa8471d2c
Andy Edinborough <andy@edinborough.org>
2011-04-14 01:08:17 -0500

    removing dependency on jQuery

--- stressTest.js
+++ stressTest.js
@@ -9,16 +9,85 @@ var stressTest = (function () {
             for (var i = 0, ii = this.length; i < ii; i++) {
                 func.call(this, this[i], i, this);
             }
+        },
+        filter = Array.prototype.filter || function (func) {
+            var ret = [];
+            forEach.call(this, function (ii) {
+                if (func(ii)) ret.push(ii);
+            });
+            return ret;
         };
 
+    function getChildren(elm) {
+        if (elm.length) {
+            var ret = [];
+            forEach.call(elm, function (ii) {
+                Array.prototype.push.apply(ret, getChildren(ii));
+            });
+            return ret;
+        }
+        return elm.all ? elm.all : elm.getElementsByTagName("*");
+    }
+
+    function getElementsByClassName(elm, className) {
+        if (elm.getElementsByClassName) return elm.getElementsByClassName(className);
+        var arrElements = getChildren(elm);
+        className = className.replace(/\-/g, "\\-");
+        var rx = new RegExp("(^|\\s)" + className + "(\\s|$)");
+        return filter.call(arrElements, function (ii) {
+            return tx.test(ii.className);
+        });
+    }
+
+    function removeClass(elms, className) {
+        var rx = new RegExp("(^|\\s)" + className + "(\\s|$)");
+        forEach.call(elms, function (ii) {
+            ii.className = (ii.className || '').replace(rx, '');
+        });
+    }
+
+    function addClass(elms, className) {
+        removeClass(elms, className);
+        forEach.call(elms, function (ii) {
+            ii.className = (ii.className || '') + ' ' + className;
+        });
+    }
+
+    function (elm, name, func) {
+        var parts = name.split('.');
+        if (!elm.__events) elm.__events = {};
+        if (!elm.__events[name]) elm.events[name] = [];
+        elm.__events[name].push(func);
+        name = parts[0];
+        if (elm.attachEvent) elm.attachEvent(name, func);
+        if (elm.addEventListener) elm.addEventListener(name, func, false);
+    }
+
+    function unbind(elm, name, func) {
+        if (!func && elm.__events && elm.__events[name]) {
+            forEach.call(elm.__events[name].splice(0), function (ii) {
+                unbind(elm, name, ii);
+            });
+        } else {
+            if (elm.detachEvent) elm.attachEvent(name, func);
+            if (elm.removeEventListener) elm.addEventListener(name, func, false);
+            if (elm.__events && elm.__events[name]) {
+                var i = elm.__events[name].indexOf(func);
+                elm.__events[name].splice(i, 1);
+            }
+        }
+    }
+
     function testClass(times, className, results, finished) {
-        var elms = $('.' + className).removeClass(className);
+        var elms = getElementsByClassName(document, className);
+        removeClass(elms, className);
+
         stress(times, function (time) {
-            elms.addClass(className);
+            addClass(elms, className);
             if (className === baselineName) results[baselineName] = time;
             else results[className] = {
                 length: elms.length,
-                affected: elms.length + elms.children().length,
+                children: getChildren(elms).length,
                 time: time,
                 delta: results[baselineName] - time
             };
@@ -28,8 +97,8 @@ var stressTest = (function () {
 
     function getAllClasses() {
         var all = {};
-        $('[class]').each(function () {
-            var classes = $(this).attr('class').split(' ');
+        forEach.call(getChildren(document), function (ii) {
+            var classes = (ii.className || '').split(' ');
             forEach.call(classes, function (ii) {
                 if (ii.length > 0 && !all[ii]) {
                     all[ii] = true;
@@ -41,20 +110,19 @@ var stressTest = (function () {
     }
 
     function stress(times, finish) {
-        var win = $(window),
-            total = 0,
+        var total = 0,
             n = 0, current = 0,
             work = function () {
                 current = +new Date;
                 window.scrollBy(0, n % 2 == 0 ? 100 : -100);
             };
 
-        win.bind('scroll.stressTest', function () {
+        bind(window, 'scroll.stressTest', function () {
             total += (+new Date) - current;
             n++;
             if (n < times) setTimeout(work, 0);
             else {
-                win.unbind('scroll.stressTest');
+                unbind(window, 'scroll.stressTest');
                 finish.call(window, total);
             }
         });
@@ -62,7 +130,7 @@ var stressTest = (function () {
         work();
     }
 
-    return function (times, finishall) {
+    function exports(times, finishall) {
         if (!times) times = 100;
         if (!finishall) finishall = function (results) {
             var worst = Object_keys(results).sort(function (a, b) {
@@ -87,4 +155,24 @@ var stressTest = (function () {
 
         testClass(times, baselineName, results, finish);
     }
+
+    exports.bookmarklet = function () {
+        var num = prompt('How many tests would you like to run?', 10);
+        if (num > 0) {
+            exports(num, function (results) {
+                var worst = Object_keys(results).sort(function (a, b) {
+                    return results[b] - results[a];
+                }).slice(0, 5);
+            });
+
+            var log = '';
+            forEach.call(worst, function (ii) {
+                log += ii + "\t" + results[ii].delta + 'ms\n';
+            });
+
+            alert(log);
+        }
+    }
+
+    return exports;
 })();
