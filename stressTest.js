0b9ecb944e73255bc583a2fe967e11ed453b16c2
Andy Edinborough <andy@edinborough.org>
2011-04-14 11:05:13 -0500

    fixed bind/unbind, rewrote addClass/removeClass, added an HTML based report rather than an alert()

--- stressTest.js
+++ stressTest.js
@@ -1,22 +1,38 @@
 var stressTest = (function () {
     var baselineName = '__STRESSTESTBASELINE__',
-        Object_keys = Object.keys || function (obj) {
-            var ret = [];
-            for (var i in obj) if (obj.hasOwnProperty(i)) ret.push(i);
-            return ret;
-        },
-        forEach = Array.prototype.forEach || function (func) {
-            for (var i = 0, ii = this.length; i < ii; i++) {
-                func.call(this, this[i], i, this);
-            }
-        },
-        filter = Array.prototype.filter || function (func) {
-            var ret = [];
-            forEach.call(this, function (ii) {
-                if (func(ii)) ret.push(ii);
-            });
-            return ret;
-        };
+    whitespace = /\s+/,
+    Object_keys = Object.keys || function (obj) {
+        var ret = [];
+        for (var i in obj) if (obj.hasOwnProperty(i)) ret.push(i);
+        return ret;
+    },
+    forEach = Array.prototype.forEach || function (func) {
+        for (var i = 0, ii = this.length; i < ii; i++) {
+            func.call(this, this[i], i, this);
+        }
+    },
+    filter = Array.prototype.filter || function (func) {
+        var ret = [];
+        forEach.call(this, function (ii) {
+            if (func(ii)) ret.push(ii);
+        });
+        return ret;
+    }
+
+    function indexClasses() {
+        var all = getChildren(document), ret = {};
+        forEach.call(all, function (elm) {
+            if (!elm.className) return;
+            forEach.call(
+                filter.call(elm.className.split(whitespace), function (n) { return n.length > 0; }),
+                function (n) {
+                    if (!ret[n]) ret[n] = [];
+                    ret[n].push(elm);
+                });
+        });
+
+        return ret;
+    }
 
     function getChildren(elm) {
         if (typeof elm.length != 'undefined') {
@@ -35,38 +51,14 @@ var stressTest = (function () {
         return elm.all ? elm.all : elm.getElementsByTagName("*");
     }
 
-    function getElementsByClassName(elm, className) {
-        if (elm.getElementsByClassName) return elm.getElementsByClassName(className);
-        var arrElements = getChildren(elm);
-        className = className.replace(/\-/g, "\\-");
-        var rx = new RegExp("(^|\\s)" + className + "(\\s|$)");
-        return filter.call(arrElements, function (ii) {
-            return tx.test(ii.className);
-        });
-    }
-
-    function removeClass(elms, className) {
-        var rx = new RegExp("(^|\\s)" + className + "(\\s|$)");
-        forEach.call(elms, function (ii) {
-            ii.className = (ii.className || '').replace(rx, '');
-        });
-    }
-
-    function addClass(elms, className) {
-        removeClass(elms, className);
-        forEach.call(elms, function (ii) {
-            ii.className = (ii.className || '') + ' ' + className;
-        });
-    }
-
     function bind(elm, name, func) {
         var parts = name.split('.');
         if (!elm.__events) elm.__events = {};
         if (!elm.__events[name]) elm.__events[name] = [];
         elm.__events[name].push(func);
         name = parts[0];
-        if (elm.attachEvent) elm.attachEvent(name, func);
-        if (elm.addEventListener) elm.addEventListener(name, func, false);
+        if (elm.attachEvent) elm.attachEvent('on' + name, func);
+        else if (elm.addEventListener) elm.addEventListener(name, func, false);
     }
 
     function unbind(elm, name, func) {
@@ -75,59 +67,78 @@ var stressTest = (function () {
                 unbind(elm, name, ii);
             });
         } else {
-            if (elm.detachEvent) elm.attachEvent(name, func);
-            if (elm.removeEventListener) elm.addEventListener(name, func, false);
+            name = name.split('.')[0];
+            if (elm.detachEvent) elm.detachEvent('on' + name, func);
+            else if (elm.removeEventListener) elm.removeEventListener(name, func, false);
             if (elm.__events && elm.__events[name]) {
                 var i = elm.__events[name].indexOf(func);
-                elm.__events[name].splice(i, 1);
+                if (i > -1) elm.__events[name].splice(i, 1);
             }
         }
+        elm = null;
     }
 
-    function testClass(times, className, results, finished) {
-        var elms = getElementsByClassName(document, className);
-        removeClass(elms, className);
+    function removeClass(elms, className) {
+        forEach.call(elms, function (ii) {
+            ii.className = distinct(
+                    filter.call(
+                        (ii.className || '').split(whitespace),
+                        function (jj) { return jj != className; }
+                    )
+                ).join(' ');
+        });
+    }
 
-        stress(times, function (time) {
+    function distinct(arr) {
+        var hash = {};
+        forEach.call(arr, function (ii) {
+            hash[ii] = true;
+        });
+        return Object_keys(hash);
+    }
+
+    function addClass(elms, className) {
+        forEach.call(elms, function (ii) {
+            var classes = (ii.className || '').split(' ');
+            classes.push(className);
+            ii.setAttribute('class', distinct(classes).join(' '));
+        });
+    }
+
+    function testClass(times, className, state, finished) {
+        var elms = state.elms[className] || [];
+        removeClass(elms, className);
+        stress(state, times, function (time) {
             addClass(elms, className);
-            if (className === baselineName) results[baselineName] = time;
-            else results[className] = {
-                length: elms.length,
-                children: getChildren(elms).length,
-                time: time,
-                delta: results[baselineName] - time
-            };
+            if (className == baselineName) {
+                state.baseTime = time;
+            } else {
+                state.results[className] = {
+                    length: elms.length,
+                    affected: elms.length + getChildren(elms).length,
+                    time: time,
+                    delta: time - state.baseTime
+                };
+            }
             finished(className, time);
         });
     }
 
-    function getAllClasses() {
-        var all = {};
-        forEach.call(getChildren(document), function (ii) {
-            var classes = (ii.className || '').split(' ');
-            forEach.call(classes, function (ii) {
-                if (ii.length > 0 && !all[ii]) {
-                    all[ii] = true;
-                }
-            });
-        });
-
-        return Object_keys(all);
-    }
-
-    function stress(times, finish) {
+    function stress(state, times, finish) {
         var total = 0,
-            n = 0, current = 0,
-            work = function () {
-                current = +new Date;
-                window.scrollBy(0, n % 2 == 0 ? 100 : -100);
-            };
+        n = 0, current = 0,
+        work = function () {
+            current = +new Date;
+            window.scrollBy(0, n % 2 == 0 ? 100 : -100);
+        };
+        times *= 2; //each test consists of scrolling down, and then back up
 
         bind(window, 'scroll.stressTest', function () {
             total += (+new Date) - current;
             n++;
-            if (n < times) setTimeout(work, 0);
-            else {
+            if (n < times && !state.cancel) {
+                setTimeout(work, 0);
+            } else {
                 unbind(window, 'scroll.stressTest');
                 finish.call(window, total);
             }
@@ -136,48 +147,60 @@ var stressTest = (function () {
         work();
     }
 
-    function exports(times, finishall) {
+    function stressTest(times, finish) {
         if (!times) times = 100;
-        if (!finishall) finishall = function (results) {
-            var worst = Object_keys(results).sort(function (a, b) {
-                return results[b] - results[a];
-            }).slice(0, 5);
 
-            forEach.call(worst, function (ii) {
-                console.log(ii + " added " + results[ii].delta + "ms");
-            });
-
-        };
-
-        var classes = getAllClasses(),
-            results = {},
-            finish = function (className, time) {
-                if (classes.length > 0) {
-                    testClass(times, classes.pop(), results, finish);
+        var state = { elms: indexClasses(), results: {} },
+            queue = Object_keys(state.elms),
+            testfinish = function (className, time) {
+                if (queue.length > 0 && !state.cancel) {
+                    testClass(times, queue.shift(), state, testfinish);
                 } else {
-                    finishall(results);
+                    unbind(document, 'keydown.stressTest');
+                    if (finish && !state.cancel) finish(state);
                 }
             };
 
-        testClass(times, baselineName, results, finish);
+        bind(document, 'keydown.stressTest', function (e) {
+            if (e.keyCode == 27) state.cancel = true;
+        });
+
+        /* just warming up */
+        testClass(times, baselineName, state, function () {
+            testClass(times, baselineName, state, testfinish);
+        });
     }
 
-    exports.bookmarklet = function () {
-        var num = prompt('How many tests would you like to run?', 10);
+    stressTest.bookmarklet = function () {
+        var num = prompt('How many tests would you like to run (# stress tests per class)?', 5);
         if (num > 0) {
-            exports(num, function (results) {
-                var log = '', worst = Object_keys(results).sort(function (a, b) {
-                    return results[b] - results[a];
-                }).slice(0, 5);
+            stressTest(num, function (state) {
+                var spaces = '', log = '<table><thead><tr><th>Class</th><th> </th><th>Delta</th><th>Total</th></tr></thead>', all = Object_keys(state.results),
+                    worst = all.sort(function (a, b) {
+                        return state.results[a].time - state.results[b].time;
+                    }).splice(0, 20);
 
                 forEach.call(worst, function (ii) {
-                    log += ii + "\t" + results[ii].delta + 'ms\n';
+                    while (ii.length > spaces.length) spaces += ' ';
                 });
 
-                alert(log);
+                forEach.call(worst, function (ii) {
+                    log += '<tr><td>Removing .' + ii + spaces.substr(ii.length) +
+                        '</td><td>' + (state.results[ii].delta < 0 ? '<span style="color:red">saves</span>' : '<span style="color:green">adds</span>') +
+                        '</td><td style="text-align:right">' + Math.abs(state.results[ii].delta) + 'ms</td><td style="text-align:right">' + state.results[ii].time + 'ms</td></tr>\n';
+                });
+
+                //alert('Baseline \t ' + state.baseTime + 'ms\n' + log);
+                var report = document.createElement('div'),
+                    style = { position: 'fixed', top: '10px', right: '10px', 'font': '12px monospace', 'z-index': 2147483647, background: 'white', padding: '2px', border: 'solid 2px #777' };
+                forEach.call(Object_keys(style), function (ii) { report.style.setProperty(ii, style[ii]); });
+                report.innerHTML = log + '</table><p>Classes Tested: ' + all.length + '</p><p>Baseline Time: ' + state.baseTime + 'ms</p>';
+                forEach.call(report.getElementsByTagName('td'), function (td) { td.style.padding = '1px'; });
+                document.body.appendChild(report);
+
             });
         }
     }
 
-    return exports;
+    return stressTest;
 })();
