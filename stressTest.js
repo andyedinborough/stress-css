2602e50dbc73611afaca8c5e1bf5ca127ef1a053
Andy Edinborough <andy@edinborough.org>
2011-04-15 10:09:20 -0500

    added more info to report; now reports time/test; also tests ID's

--- stressTest.js
+++ stressTest.js
@@ -17,36 +17,46 @@ var stressTest = (function () {
             if (func(ii)) ret.push(ii);
         });
         return ret;
+    };
+    
+    function style(elm, props){
+      forEach.call(Object_keys(props), 
+        function (ii) { 
+          try { 
+            elm.style.setProperty(ii, props[ii]); 
+          } catch(x) { } 
+        }
+      );
     }
 
-    function indexClasses() {
+    function indexElements() {
         var all = getChildren(document), ret = {};
         forEach.call(all, function (elm) {
-            if (!elm.className) return;
-            forEach.call(
-                filter.call(elm.className.split(whitespace), function (n) { return n.length > 0; }),
-                function (n) {
-                    if (!ret[n]) ret[n] = [];
-                    ret[n].push(elm);
-                });
+            if (elm.className) {
+              forEach.call(
+                  filter.call(elm.className.split(whitespace), function (n) { return n.length > 0; }),
+                  function (n) {
+                      if (!ret['.'+n]) ret['.'+n] = [];
+                      ret['.'+n].push(elm);
+                  });
+            } else if(elm.id) {
+              if (!ret['#'+elm.id]) ret['#'+elm.id] = [];
+                ret['#'+elm.id].push(elm);
+            }
         });
 
         return ret;
-    }
+    } 
 
     function getChildren(elm) {
         if (typeof elm.length != 'undefined') {
-            var all = [], ret = [], hash = {};
+            var all = []; //, ret = [], hash = {};
             forEach.call(elm, function (ii) {
                 Array.prototype.push.apply(all, Array.prototype.slice.call(getChildren(ii), 0));
             });
-            forEach.call(all, function (ii) {
-                if (!hash[ii]) {
-                    hash[ii] = true;
-                    ret.push(ii);
-                }
-            });
-            return ret;
+          
+            //need a way to make sure the list is distinct
+            return all; //ret;
         }
         return elm.all ? elm.all : elm.getElementsByTagName("*");
     }
@@ -101,26 +111,40 @@ var stressTest = (function () {
         forEach.call(elms, function (ii) {
             var classes = (ii.className || '').split(' ');
             classes.push(className);
-            ii.setAttribute('class', distinct(classes).join(' '));
+            ii.className = distinct(classes).join(' ');
         });
     }
+    
+    function removeSelector(elms, selector){
+      var which = selector.substr(0, 1);
+      selector = selector.substr(1);
+      if(which === '.') removeClass(selector);
+      else if(which === '#') forEach.call(elms, function(elm){ elm.attributes.removeNamedItem('id'); });
+    }
+    
+    function addSelector(elms, selector){
+      var which = selector.substr(0, 1);
+      selector = selector.substr(1);
+      if(which === '.') addClass(selector);
+      else if(which === '#') forEach.call(elms, function(elm){ elm.id = selector; });
+    }
 
-    function testClass(times, className, state, finished) {
-        var elms = state.elms[className] || [];
-        removeClass(elms, className);
+    function testSelector(times, selector, state, finished) {
+        var elms = state.elms[selector] || [];
+        removeSelector(elms, selector);
         stress(state, times, function (time) {
-            addClass(elms, className);
-            if (className == baselineName) {
+            addSelector(elms, selector);
+            if (selector == baselineName) {
                 state.baseTime = time;
             } else {
-                state.results[className] = {
+                state.results[selector] = {
                     length: elms.length,
-                    affected: elms.length + getChildren(elms).length,
+                    children: getChildren(elms).length,
                     time: time,
                     delta: time - state.baseTime
                 };
             }
-            finished(className, time);
+            finished(selector, time);
         });
     }
 
@@ -137,7 +161,7 @@ var stressTest = (function () {
                 setTimeout(work, 0);
             } else {
                 unbind(window, 'scroll.stressTest');
-                finish.call(window, (+new Date) - now);
+                finish((+new Date) - now);
             }
         });
 
@@ -147,11 +171,11 @@ var stressTest = (function () {
     function stressTest(times, finish) {
         if (!times) times = 100;
 
-        var state = { elms: indexClasses(), results: {} },
+        var state = { elms: indexElements(), results: {} },
             queue = Object_keys(state.elms),
             testfinish = function (className, time) {
                 if (queue.length > 0 && !state.cancel) {
-                    testClass(times, queue.shift(), state, testfinish);
+                    testSelector(times, queue.shift(), state, testfinish);
                 } else {
                     unbind(document, 'keydown.stressTest');
                     if (finish && !state.cancel) finish(state);
@@ -164,34 +188,85 @@ var stressTest = (function () {
 
         window.scrollTo(0, 0); //the first test scrolls down
         /* just warming up */
-        testClass(times, baselineName, state, function () {
-            testClass(times, baselineName, state, testfinish);
+        testSelector(times, baselineName, state, function () {
+            testSelector(times, baselineName, state, testfinish);
         });
     }
 
+    function formatNumber(number, leading, trailing){
+      leading = leading || 0;
+      trailing = trailing || 2;
+      var parts = (number+'.').split('.'); 
+      while(parts[0].length<leading) parts[0] = '0' + parts[0];
+      if(trailing < 1) parts[1] = '';
+      else if(parts[1].length > trailing) parts[1] = parts[1].substr(0, trailing);
+      else while(parts[1].length < trailing) parts[1] += '0';                   
+      return parts[0] + (parts[1].length > 0 ? ('.' + parts[1]) : '');
+    }
+
     stressTest.bookmarklet = function () {
-        var num = prompt('How many tests would you like to run (# stress tests per class)?', 5);
+        if(stressTest.report) stressTest.report.close();
+        
+        var num = prompt('How many tests would you like to run (# stress tests per selector)?', 5);
         if (num > 0) {
             stressTest(num, function (state) {
-                var log = '<table><thead><tr><th>Class</th><th> </th><th>Delta</th><th>Total</th></tr></thead>', 
+                var log = '<table><thead><tr><th>Selector</th><th># Elms.</th><th># Child.</th><th> </th><th>Delta</th><th>Total</th></tr></thead>', 
+                    report = document.createElement('div'),
+                    close = document.createElement('a'),
                     all = Object_keys(state.results),
                     worst = all.sort(function (a, b) {
                         return state.results[a].time - state.results[b].time;
                     }).slice(0, 20);
 
                 forEach.call(worst, function (ii) {
-                    log += '<tr><td>Removing <strong>' + ii +
-                        '</strong></td><td>' + (state.results[ii].delta < 0 ? '<span style="color:red">saves</span>' : '<span style="color:green">adds</span>') +
-                        '</td><td style="text-align:right">' + Math.abs(state.results[ii].delta) + 'ms</td><td style="text-align:right">' + state.results[ii].time + 'ms</td></tr>\n';
+                    log += '<tr><td>Removing <strong style="font:12px monospace">' + ii +
+                        '</strong></td><td style="text-align:right; font:12px monospace">' + state.results[ii].length + '</td><td style="text-align:right; font:12px monospace">' + state.results[ii].children + 
+                        '</td><td style="text-align:right">' + (state.results[ii].delta < 0 ? '<span style="color:red">saves</span>' : '<span style="color:green">adds</span>') +
+                        '</td><td style="text-align:right; font:12px monospace">' + formatNumber(Math.abs(state.results[ii].delta)/num) + 'ms</td><td style="text-align:right; font:12px monospace">' + 
+                        formatNumber(state.results[ii].time/num) + 'ms</td></tr>\n';
                 });
- 
-                var report = document.createElement('div'),
-                    style = { position: 'fixed', top: '10px', right: '10px', 'font': '12px monospace', 'z-index': 999999999, background: 'white', padding: '2px', border: 'solid 2px #777' };
-                forEach.call(Object_keys(style), function (ii) { try { report.style.setProperty(ii, style[ii]); } catch(x) { } });
-                report.innerHTML = log + '</table><p>Classes Tested: ' + all.length + '</p><p>Baseline Time: ' + state.baseTime + 'ms</p>';
-                forEach.call(report.getElementsByTagName('td'), function (td) { td.style.padding = '1px'; });
-                document.body.appendChild(report);
-
+                log += '</table><hr/><table><tr><td style="text-align:right">Classes Tested:</td><td style="font:12px monospace">' + all.length + '</td></tr>' +
+                  '<tr><td style="text-align:right">Baseline Time:</td><td style="font:12px monospace">' + formatNumber(state.baseTime/num) + 'ms</td></tr>' +
+                  '<tr><td style="text-align:right">Num. Tests:</td><td style="font:12px monospace">' + num + '</td></tr>';
+                
+                if(filter.call(all, function(cn){
+                  return state.results[cn].time <= 15;
+                }).length) {
+                  log += '<tr><td style="color:red; text-align:right;font-weight:bold">Warning:</td><td>Increase the number<br />of tests to get more<br />accurate results</td></tr>';
+                }
+                
+                style(report, { 
+                  position: 'fixed', top: '10px', right: '10px', 
+                  font: '12px Helvetica,Arials,sans-serif', 'z-index': 999999999, 
+                  background: 'white', padding: '2px', 
+                  border: 'solid 2px #777' 
+                });
+                                
+                report.innerHTML = log + '</table>';
+                forEach.call(
+                  report.getElementsByTagName('td'), 
+                  function (td) { 
+                    style(td, { padding: '1px', 'vertical-align': 'top' }); 
+                  }
+                );
+                
+                close.innerHTML = '&#215;';
+                style(close, { 
+                  position: 'absolute', top: 0, right: 0,
+                  'text-decoration': 'none', 'font-weight': 'bold',
+                  cursor: 'pointer', color: 'red', 
+                  'font-size': '1.3em', 'line-height': '8px' 
+                });
+                report.close = function(){
+                  report.parentNode.removeChild(report);
+                  unbind(close, 'click');
+                  stressTest.report = null;                             
+                };
+                bind(close, 'click', report.close);
+                
+                report.appendChild(close);
+                document.body.appendChild(report);   
+                stressTest.report = report;                             
             });
         }
     }
